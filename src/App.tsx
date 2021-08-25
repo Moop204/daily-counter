0755e586a66f4aad7a7cd1fad1f8e034b4edaa9a
Moop204 <andrew.nguyen2045@outlook.com>
2021-08-25 21:52:54 +1000

    added tasks that require multiple actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import { FunctionComponent, useEffect, useState } from "react";
-import { Card, Col, Container, Row, ToggleButton } from "react-bootstrap";
+import {
+  Badge,
+  Button,
+  Card,
+  Col,
+  Container,
+  Row,
+  ToggleButton,
+} from "react-bootstrap";
 
 interface ITask {
   description: string;
@@ -56,6 +64,62 @@ const CheckTask: FunctionComponent<ICheckTask> = ({
   );
 };
 
+interface IMultiTask extends ITask {
+  initialValue: number;
+  totalValue: number;
+}
+
+const MultiTask: FunctionComponent<IMultiTask> = ({
+  description,
+  initialValue,
+  totalValue,
+}) => {
+  const [count, setCount] = useState(initialValue);
+  const [bgColour, setBgColour] = useState(
+    initialValue ? "#A7F1A8" : "#F5F5F5"
+  );
+
+  useEffect(() => {
+    setBgColour(count >= totalValue ? "#A7F1A8" : "#F5F5F5");
+  }, [count, totalValue]);
+
+  return (
+    <Card style={{ backgroundColor: bgColour }}>
+      <Card.Body>
+        <Row>
+          <Col
+            className="col-xs-3 col-sm-5 col-md-7 col-lg-9"
+            style={{ fontSize: "x-large", textOverflow: "ellipses" }}
+          >
+            {description}
+          </Col>
+          <Col xs="auto">
+            <Button
+              className={
+                count === totalValue
+                  ? "mb-2 active .btn-outline-success"
+                  : "mb-2"
+              }
+              id="multi-button"
+              variant="outline-success"
+              onClick={() => {
+                const newCount = count + 1;
+                setCount(newCount > totalValue ? totalValue : newCount);
+                console.log(count);
+              }}
+            >
+              Complete{" "}
+              <Badge style={{ backgroundColor: "#A7F1A8" }}>
+                {count}/{totalValue}
+              </Badge>
+            </Button>
+          </Col>
+        </Row>
+      </Card.Body>{" "}
+    </Card>
+  );
+};
+
 function App() {
   const [checked, setChecked] = useState(false);
   const [bgColour, setBgColour] = useState("light");
@@ -69,7 +133,7 @@ function App() {
       <h1>Daily Counters</h1>
       <Row>
         <Col>
-          <CheckTask description="Make your bed" initialValue={false} />
+          <MultiTask description="Eat food " initialValue={0} totalValue={3} />
         </Col>
         <Col>
           <CheckTask description="Make your bed" initialValue={false} />
